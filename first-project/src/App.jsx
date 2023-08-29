6cd83479a13aa8cf831e9a478a7c7606b9e572a5
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-29 10:38:14 +0200

    Creating first components

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,10 +1,12 @@
+import { MyName } from './MyName';
+import { TodoList } from './TodoList';
+
 function App() {
   return (
-    <div className='large' id='largerDiv'>
-      <label htmlFor='inputId'>This is a label</label>
-      <input id='inputId' type='number' defaultValue={3} />
+    <div>
+      <MyName />
+      <TodoList />
     </div>
   );
 }
-
 export default App;
