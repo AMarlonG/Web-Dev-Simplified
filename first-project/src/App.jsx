eb66c5865466783ae60cbf97cb61ea394dafefad
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-29 10:57:55 +0200

    Adding props, specific and children

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,11 +1,15 @@
 import { MyName } from './MyName';
+import { PropPlay } from './PropPlay';
 import { TodoList } from './TodoList';
+import { TodoListItem } from './TodoListItem';
 
 function App() {
   return (
     <div>
       <MyName />
       <TodoList />
+      <PropPlay name='Marlon' age={30} gender='m' />
+      <TodoListItem isCompleted={true}>Learn React</TodoListItem>
     </div>
   );
 }
