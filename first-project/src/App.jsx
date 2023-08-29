fdaa1bf5d869bdeffe526f158653f4acd7e64a24
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-29 10:22:19 +0200

    First lesson code

---
+++ first-project/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className='large' id='largerDiv'>
+      <label htmlFor='inputId'>This is a label</label>
+      <input id='inputId' type='number' defaultValue={3} />
+    </div>
+  );
+}
+
+export default App;
