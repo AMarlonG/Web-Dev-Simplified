ce4a85691742e72cd29a1534d63145deb75ab8b8
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-30 18:03:44 +0200

    Finished counter with name project

--- Projects/Counter With Name/src/CounterWithName.jsx
+++ Projects/Counter With Name/src/CounterWithName.jsx
@@ -1,21 +1,36 @@
-import useState from 'react';
+import { useState } from 'react';
 
 export function CounterWithName() {
-  //   const [age, setAge] = useState(0);
-  //   const [name, setName] = useState('Your Name');
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState('');
 
   return (
     <>
       <h1>Counter With Name</h1>
       <div>
-        <button>-</button>
-        <span>Age</span>
-        <button>+</button>
+        <button
+          onClick={
+            age === 0
+              ? () => setAge(0)
+              : () => setAge((currentAge) => currentAge - 1)
+          }
+        >
+          Decrease
+        </button>
+        <span> {age} </span>
+        <button onClick={() => setAge((currentAge) => currentAge + 1)}>
+          Increase
+        </button>
       </div>
 
       <br />
-      <input />
-      <h2>My name is name, and I am age years old.</h2>
+      <input value={name} onChange={(event) => setName(event.target.value)} />
+
+      <h2>
+        My name is {name}, and I am {age} years old.
+      </h2>
     </>
   );
 }
+
+// onClick = {counter === 0 ? () => setCounter(1) : () => setCounter(0)};
