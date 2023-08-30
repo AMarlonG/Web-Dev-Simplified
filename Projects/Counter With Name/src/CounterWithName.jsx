f0ebc4ca9ba62938e88e004bcd65fa39188743e3
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-08-30 18:55:42 +0200

    yep

--- Projects/Counter With Name/src/CounterWithName.jsx
+++ Projects/Counter With Name/src/CounterWithName.jsx
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export function CounterWithName() {
   const [age, setAge] = useState(0);
-  const [name, setName] = useState('');
+  const [name, setName] = useState('Enter name');
+
+  // This allows for changes (side effects) based on a change in states of this component
+  useEffect(() => {
+    console.log('useEffect called and age is', age);
+  }, [age]);
+  // age inside of the array at the end is the dependency array
+  // It means this useEffect will only run if, and every time, the age state changes
+  // If the dependency array is empty, then the useEffect will only run the first time the component is rendered
 
   return (
     <>
@@ -32,5 +40,3 @@ export function CounterWithName() {
     </>
   );
 }
-
-// onClick = {counter === 0 ? () => setCounter(1) : () => setCounter(0)};
