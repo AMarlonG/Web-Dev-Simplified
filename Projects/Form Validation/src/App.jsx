7706861cc3c691add37eeb2188c2713a1a6fc2b8
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-09-11 18:39:30 +0200

    Finished From project, but not working

--- Projects/Form Validation/src/App.jsx
+++ Projects/Form Validation/src/App.jsx
@@ -1,40 +1,9 @@
+// Using form is best for accessibility, but it does come with some caveats
+// One issue is that the default behavior of the form is to refresh the page, and re-render the component
+// This is avoided by using the onSubmit event handler on the form element, and calling event.preventDefault()
+import { StateForm } from './StateForm';
 import './styles.css';
 
-function App() {
-  return (
-    <>
-      <h1>Form Validation</h1>
-      <form className='form'>
-        <div className='form-group-error'>
-          <label htmlFor='email' className='label'>
-            Email
-          </label>
-          <br />
-          <input
-            type='email'
-            className='input'
-            id='email'
-            value='test@test.com'
-          />
-          <div className='msg'>Must end with @webdevsimplified.com</div>
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password' className='label'>
-            Password
-          </label>
-          <input
-            type='password'
-            className='input'
-            id='password'
-            value='Password123!'
-          />
-        </div>
-        <button className='btn' type='submit'>
-          Submit
-        </button>
-      </form>
-    </>
-  );
+export default function App() {
+  return <StateForm />;
 }
-
-export default App;
