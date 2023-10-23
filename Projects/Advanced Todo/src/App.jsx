ab8f57323d1a3c7fcfd1c477b9c23bab455cb0b6
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-10-23 18:36:47 +0200

    Added a reducer

--- Projects/Advanced Todo/src/App.jsx
+++ Projects/Advanced Todo/src/App.jsx
@@ -1,65 +1,70 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import './styles.css';
 import { TodoItem } from './TodoItem';
+import { NewTodoForm } from './NewTodoForm';
 
 const LOCAL_STORAGE_KEY = 'TODOS';
+const ACTIONS = {
+  ADD_TODO: 'add_todo',
+  UPDATE_TODO: 'update_todo',
+  TOGGLE_TODO: 'toggle_todo',
+  DELETE_TODO: 'delete_todo',
+};
+
+// Create a reducer function that takes in two arguments, the first one is the current state, and the second one is the action that we want to perform on that state.
+function reducer(todos, { type, payload }) {
+  if (type === 'add_todo') {
+    return [
+      ...todos,
+      { name: payload.name, completed: false, id: crypto.randomUUID() },
+    ];
+  }
+
+  if (type === 'toggle_todo') {
+    return todos.map((todo) => {
+      if (todo.id === payload.id)
+        return { ...todo, completed: payload.completed };
+
+      return todo;
+    });
+  }
+
+  if (type === 'delete_todo') {
+    return todos.filter((todo) => todo.id !== payload.id);
+  }
+
+  throw new Error(`Unhandled action type: ${type}.`);
+}
 
 function App() {
-  const [newTodoName, setNewTodoName] = useState('');
-
-  const [todos, setTodos] = useState(() => {
+  // Save todos to local storage with useState and useEffect
+  const [todos, dispatch] = useReducer(reducer, [], (initialValue) => {
     const value = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (value == null) return [];
+    if (value == null) return initialValue;
     return JSON.parse(value);
   });
 
-  // Save todos to local storage
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
   }, [todos]);
 
-  function addNewTodo() {
-    if (newTodoName === '') return;
-
-    setTodos((currentTodos) => {
-      return [
-        ...currentTodos,
-        { name: newTodoName, completed: false, id: crypto.randomUUID() },
-      ];
-    });
-    setNewTodoName('');
+  function addNewTodo(name) {
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name } });
   }
 
   function toggleTodo(todoId, completed) {
-    setTodos((currentTodos) => {
-      return currentTodos.map((todo) => {
-        if (todo.id === todoId) return { ...todo, completed };
-
-        return todo;
-      });
-    });
+    dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todoId, completed } });
   }
 
   function deleteTodo(todoId) {
-    setTodos((currentTodos) => {
-      return currentTodos.filter((todo) => todo.id !== todoId);
-    });
+    dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todoId } });
   }
 
   return (
     <>
       <h1>Advanced Todo App</h1>
 
-      <div id='new-todo-form'>
-        <label htmlFor='todo-input'>New Todo</label>
-        <input
-          type='text'
-          id='todo-input'
-          value={newTodoName}
-          onChange={(e) => setNewTodoName(e.target.value)}
-        />
-        <button onClick={addNewTodo}>Add Todo</button>
-      </div>
+      <NewTodoForm addNewTodo={addNewTodo} />
 
       <ul id='list'>
         {todos.map((todo) => {
