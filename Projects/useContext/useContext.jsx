05f00f21e18a57cc34d9972f10e542e87ef6f7a7
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-10-23 14:44:43 +0200

    Commenting on the use case for useContext.

---
+++ Projects/useContext/useContext.jsx
@@ -0,0 +1,3 @@
+// useContext is a hook that allows you to use the context of a component when you are not inside the component. An example is toggling a theme from a button that is not inside the component that is using the theme, such as a button toggling between light and dark mode.
+// useContext helps you avoid prop drilling, which is passing props through components that don't need the props, just to get to the component that needs the props.
+// Together with useReducer, useContext can be used to create a global state that can be accessed from anywhere in the app.
