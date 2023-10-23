2867962d1e22faa3a73228b6ec164072aa188115
Ådne M. Gulbrandsen <98260184+AMarlonG@users.noreply.github.com>
2023-10-23 17:35:56 +0200

    Initialized project and saving to local storage

---
+++ Projects/Advanced Todo/src/App.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import './styles.css';
+import { TodoItem } from './TodoItem';
+
+const LOCAL_STORAGE_KEY = 'TODOS';
+
+function App() {
+  const [newTodoName, setNewTodoName] = useState('');
+
+  const [todos, setTodos] = useState(() => {
+    const value = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (value == null) return [];
+    return JSON.parse(value);
+  });
+
+  // Save todos to local storage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addNewTodo() {
+    if (newTodoName === '') return;
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { name: newTodoName, completed: false, id: crypto.randomUUID() },
+      ];
+    });
+    setNewTodoName('');
+  }
+
+  function toggleTodo(todoId, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === todoId) return { ...todo, completed };
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(todoId) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== todoId);
+    });
+  }
+
+  return (
+    <>
+      <h1>Advanced Todo App</h1>
+
+      <div id='new-todo-form'>
+        <label htmlFor='todo-input'>New Todo</label>
+        <input
+          type='text'
+          id='todo-input'
+          value={newTodoName}
+          onChange={(e) => setNewTodoName(e.target.value)}
+        />
+        <button onClick={addNewTodo}>Add Todo</button>
+      </div>
+
+      <ul id='list'>
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
